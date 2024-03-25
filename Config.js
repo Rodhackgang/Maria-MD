77d314c36551938e50f22799d3fe3f6e808dfe3f
Rodhackgang <116571045+Rodhackgang@users.noreply.github.com>
2024-03-25 02:35:03 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['77701726']
+global.ownername = "💙𓆩‎Rodhackgang Dev𓆪_✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
